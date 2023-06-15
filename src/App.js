fd43c80574924e0b7bf1d117bb824d0e46e18547
Sandro Ptak <sandroptak@gmail.com>
2023-06-15 15:51:26 -0300

    Estruturando o projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+
+import Container from './components/layout/Conteiner'
+
 function App() {
   return (
-    <div>
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Ínicio</Link>
+        <Link to="/company">Sobre</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} > </Route>
+          <Route path="company" element={<Company />} > </Route>
+          <Route path="contact" element={<Contact />} > </Route>
+          <Route path="newproject" element={<NewProject />} > </Route>
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>Ínicio</h1>
+}
+
+export default Home
